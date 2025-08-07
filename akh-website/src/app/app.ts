731c109a9463eaa454c27202b01ce0b5357970c3
anis <a.khenissi@intiqaal.com>
2025-08-07 09:32:20 +0300

    new project structure

--- akh-website/src/app/app.ts
+++ akh-website/src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('slatech');
+  protected readonly title = signal('akh-website');
 }
